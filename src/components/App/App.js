3cc425cd266cbe5ff0598534b4aedc10f675609b
YoungJu Choi <yjchoiofficial@gmail.com>
2023-08-06 17:50:15 -0700

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from './App.module.css';
+
+import BusinessList from "../BusinessList/BusinessList";
+import SearchBar from "../SearchBar/SearchBar";
+
+const App = () => {
+  return (
+    <div className={styles.App}>
+      <h1>ravenoous</h1>
+      <SearchBar />
+      <BusinessList />
+    </div>
+  );
+};
+
+export default App; 
+
